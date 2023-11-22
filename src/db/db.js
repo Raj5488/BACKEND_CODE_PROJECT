1343539f1baca423ec0aae9196a1d6fb31c7a9db
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2023-11-22 12:52:01 +0530

    all setup configured

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ const connetDB = async () => {
         try {
             const connectionInstance = await            
                 mongoose.connect(`${process.env.
-                    MONGODB_URI}/${DB_NAME}`)
+                    MONGODB_URI}/${DB_NAME}`);
                 console.log(`\n Mongodb connected !! DB HOST:
                 ${connectionInstance.connection.host}`)
         } catch (error) {
